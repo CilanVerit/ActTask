227e505546683a050a6d11116b4aa5de36b47e36
CilanVerit <verityluis@gmail.com>
2026-03-10 16:56:48 +0700

    Removed branch and configure PostgreSQL for SQLite version for simplicity

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-jwt-secret")
 
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "sqlite:///acttask.db"
+        "DATABASE_URL", "postgresql://postgres:myacttask@localhost:5432/acttask"
         # "postgresql://postgres:myacttask@localhost:5432/acttask" - PostgreSQL version
         # "sqlite:///acttask.db" - SQLite version
     )
